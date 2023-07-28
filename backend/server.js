9fab6a7bd322c429e74b076325a5142c3367c5f1
Sabarivasan Velayutham <84288319+Sabarivasan-Velayutham@users.noreply.github.com>
2023-07-28 15:41:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,12 @@ const app = express()
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,console.log(`server started on port ${PORT}`))
-
-app.get("/",(req,res) => {
-    res.send("API is running ... ") 
-})
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`
+    )
+);
 
 app.use("/api/users",userRoutes);
 app.use("/api/notes",noteRoutes);
